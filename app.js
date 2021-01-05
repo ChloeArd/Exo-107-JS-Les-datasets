9c01c9e2985c3e1820fdd9c210adcce724f60676
ChloeArd <chloe.ardoise@gmail.com>
2021-01-05 16:25:49 +0100

    Exercice 107

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let booleen = p4.dataset.booleen;
 
 p1.innerHTML += " : " + number;
 p2.innerHTML += " : " + string.length;
-liste = ["chocolat", "fraise", "poireau"];
+liste = [liste];
 p3.innerHTML += " : " + liste;
 if (booleen === "true"){
     p4.innerHTML += " : " + "La valeur est vraie";
@@ -22,7 +22,27 @@ else {
 
 // Elements pour lesquels vous créerez des datasets depuis JavaScript.
 let p5 = document.getElementById('p5');
+p5.dataset.number2 = "10";
+p5.innerHTML += "-> " + p5.dataset.number2;
+
 let p6 = document.getElementById('p6');
+let string2 = p6.dataset.string2 = "blablacar";
+p6.innerHTML += "-> " + string2.length;
+
 let p7 = document.getElementById('p7');
+p7.dataset.liste2 = ["chocolat", "fraise", "poireau"];
+p7.innerHTML += "-> " + p7.dataset.liste2;
+
 let p8 = document.getElementById('p8');
+let booleen2 = p8.dataset.booleen2 = "true";
+if (booleen2 === "true"){
+    p8.innerHTML += "-> " + "La valeur est vraie";
+}
+else {
+    p8.innerHTML += "-> " + "La valeur n'est pas vraie";
+}
+
+
+
+
 
